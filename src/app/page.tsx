2a0801caba47366cce4b22326e2546d1149245c3
LeoPham <hongthaipro@gmail.com>
2023-11-11 15:52:29 +0700

    Section 1 2 landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,68 @@
-import { redirect } from "next/navigation";
+import Logo from "@/components/UI/Logo";
+import Section1 from "@/components/sections/Section1";
+import Section2 from "@/components/sections/Section2";
 
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+
+const navigation = [
+  { name: "Explorer", href: "/explorer" },
+  { name: "About", href: "/about" },
+  { name: "My Profile", href: "/profile" },
+];
 const LandingPage = () => {
-  redirect("/profile");
+  // redirect("/profile");
+  return (
+    <>
+      <header className="min-h-fit">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">AlgoPass</span>
+              <Logo />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              // onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <Section1 />
+        <Section2 />
+      </main>
+    </>
+  );
 };
 
 export default LandingPage;
